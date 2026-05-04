599c8871cec23719916960e31393c85d3430c545
GyulyVGC <gyulyvgc99@gmail.com>
2026-05-04 17:46:54 +0200

    include required shell deps in linux-setup

--- linux-setup.sh
+++ linux-setup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+sudo apt-get update && \
+sudo apt-get install -y iptables conntrack openvswitch-switch && \
 git pull && \
 cargo xtask build --release && \
 sudo cp nullnet-client.service /etc/systemd/system/ && \

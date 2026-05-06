718d8bae114ffb797a8eb813a098e44a61df28ee
GyulyVGC <gyulyvgc99@gmail.com>
2026-05-06 13:57:14 +0200

    add bpf-linker dep in linux-setup.sh

--- linux-setup.sh
+++ linux-setup.sh
@@ -2,6 +2,7 @@
 
 sudo apt-get update && \
 sudo apt-get install -y iptables conntrack openvswitch-switch && \
+cargo install bpf-linker && \
 git pull && \
 cargo xtask build --release && \
 sudo cp nullnet-client.service /etc/systemd/system/ && \

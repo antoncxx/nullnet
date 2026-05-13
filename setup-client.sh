d29ddbebefb481efa0f61e3f54858195712f9377
GyulyVGC <gyulyvgc99@gmail.com>
2026-05-13 11:37:04 +0200

    install protoc in setup scripts only if it's missing

--- setup-client.sh
+++ setup-client.sh
@@ -2,10 +2,11 @@
 
 sudo apt-get update && \
 sudo apt-get install -y iptables conntrack openvswitch-switch unzip && \
-curl -OL https://github.com/google/protobuf/releases/download/v3.20.3/protoc-3.20.3-linux-x86_64.zip && \
-unzip -o protoc-3.20.3-linux-x86_64.zip -d protoc3 && \
-sudo mv protoc3/bin/* /usr/local/bin/ && \
-sudo mv protoc3/include/* /usr/local/include/ && \
+{ command -v protoc >/dev/null || { \
+  curl -OL https://github.com/google/protobuf/releases/download/v3.20.3/protoc-3.20.3-linux-x86_64.zip && \
+  unzip -o protoc-3.20.3-linux-x86_64.zip -d protoc3 && \
+  sudo mv protoc3/bin/* /usr/local/bin/ && \
+  sudo mv protoc3/include/* /usr/local/include/ ; }; } && \
 cargo install cargo-binstall && \
 cargo binstall -y bpf-linker && \
 git pull && \
